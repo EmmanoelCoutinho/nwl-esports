672a81cc3c1d5c30ab2153309f116a12e8f6a8d9
EmmanoelCoutinho <coutinho.codes@gmail.com>
2022-09-15 22:33:28 -0300

    página inicial pronta

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,94 @@
+import { MagnifyingGlassPlus } from 'phosphor-react';
+
 import './styles/main.css';
 
+import logoImg from './assets/logo.svg';
+
 function App() {
-  return <div className="w-8 h-8 bg-black">das</div>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="Logo Nlw" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{' '}
+        <span className="text-transparent bg-duo-gradient bg-clip-text">
+          duo{' '}
+        </span>
+        está aqui.
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-6.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League Of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-1.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League Of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-2.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League Of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-3.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League Of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-4.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League Of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-5.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League Of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+      <div className="pt-1 mt-8 bg-duo-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-1">
+            <MagnifyingGlassPlus size={24} />
+            Publicar Anúncio
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
